c9cbd76de99b761e816bb08b82f01577d4b50781
vikharevam <ViharevAMIP@gmail.com>
2023-05-05 11:11:46 +0300

    Setting up elements

--- WeatherAPP/Models/WeatherModel.swift
+++ WeatherAPP/Models/WeatherModel.swift
@@ -14,6 +14,14 @@ struct WeatherModel {
     let cityName: String
     let temperature: Double
     
+    
+    
+    var temperatureString: String {
+    
+        return String(format: "%.1f", temperature)
+    
+    }
+    
     var conditionName: String {
         switch condition {
         case 200...232:
@@ -29,7 +37,7 @@ struct WeatherModel {
         case 800:
             return "sun.max"
         case 801...804:
-            return "cloud.bolt"
+            return "cloud.sun"
         default:
             return "cloud"
     
